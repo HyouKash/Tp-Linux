907b6b66df4c17404b4c24f579c1576197f2d204
HyouKash <HyouKa14@protonmail.com>
2021-11-17 14:51:24 +0100

    TP3

--- TP3/srv/yt/yt.sh
+++ TP3/srv/yt/yt.sh
@@ -2,11 +2,11 @@
 
 if [[ -d downloads && -d /var/log/yt ]]; then
         titlename=`youtube-dl -e $1`
-        echo "Video $1 was downloaded."
         mkdir downloads/"${titlename}"
         cd "downloads/${titlename}" && youtube-dl -q -f mp4 -o "${titlename}.mp4" $1 2>/dev/null
-        youtube-dl -q --write-description --skip-download --youtube-skip-dash-manifest -o downloads/"${titlename}"/"desc" $1 2>/dev/null
-        mv downloads/"${titlename}"/desc.description downloads/"${titlename}"/description
+	echo "Video $1 was downloaded."
+        youtube-dl -q --write-description --skip-download --youtube-skip-dash-manifest -o "desc" $1 2>/dev/null
+        mv desc.description description
         echo File Path : /srv/yt/downloads/"${titlename}"/"${titlename}"
         sudo echo "[`date "+%D %T"`] Video $1 was downloaded. File Path : /srv/yt/downloads/"${titlename}"/"${titlename}"" >> /var/log/yt/download.log
 else
