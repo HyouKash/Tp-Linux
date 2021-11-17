7330171506785dda6814ea13619b360ff0ab3c87
HyouKash <HyouKa14@protonmail.com>
2021-11-17 14:46:02 +0100

    TP3

--- TP3/srv/yt/yt.sh
+++ TP3/srv/yt/yt.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 if [[ -d downloads && -d /var/log/yt ]]; then
-        titlename=`youtube-dl --get-filename -o "%(title)s.mp4" $1`
+        titlename=`youtube-dl -e $1`
         echo "Video $1 was downloaded."
-        mkdir downloads/"${titlename::-4}"
-        youtube-dl -q -o downloads/"${titlename::-4}"/"%(title)s.mp4" $1 2>/dev/null
-        youtube-dl -q --write-description --skip-download --youtube-skip-dash-manifest -o downloads/"${titlename::-4}"/"desc" $1 2>/dev/null
-        mv downloads/"${titlename::-4}"/desc.description downloads/"${titlename::-4}"/description
-        echo File Path : /srv/yt/downloads/"${titlename::-4}"/"${titlename}"
-        sudo echo "[`date "+%D %T"`] Video $1 was downloaded. File Path : /srv/yt/downloads/"${titlename::-4}"/"${titlename}"" >> /var/log/yt/download.log
+        mkdir downloads/"${titlename}"
+        cd "downloads/${titlename}" && youtube-dl -q -f mp4 -o "${titlename}.mp4" $1 2>/dev/null
+        youtube-dl -q --write-description --skip-download --youtube-skip-dash-manifest -o downloads/"${titlename}"/"desc" $1 2>/dev/null
+        mv downloads/"${titlename}"/desc.description downloads/"${titlename}"/description
+        echo File Path : /srv/yt/downloads/"${titlename}"/"${titlename}"
+        sudo echo "[`date "+%D %T"`] Video $1 was downloaded. File Path : /srv/yt/downloads/"${titlename}"/"${titlename}"" >> /var/log/yt/download.log
 else
 	if [[ -d /var/log/yt ]]; then
 		echo "Dossier downloads manquant, error.."                             
