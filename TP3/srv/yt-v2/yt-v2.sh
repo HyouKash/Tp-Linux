248b953937bdc468469c328e58f0a9005a4c7b2d
HyouKash <HyouKa14@protonmail.com>
2021-11-19 09:04:17 +0100

    TP3

--- TP3/srv/yt-v2/yt-v2.sh
+++ TP3/srv/yt-v2/yt-v2.sh
@@ -3,16 +3,24 @@
 if [[ -d downloads && -d /var/log/yt ]]; then
 	bypass=`pwd`
 	for i in `cat file.txt | xargs`; do
-        	titlename=`youtube-dl -e $i`
-        	mkdir downloads/"${titlename}"
-        	cd "downloads/${titlename}" && youtube-dl -q -f mp4 -o "${titlename}.mp4" $i 2>/dev/null
-		echo "Video $i was downloaded."
-        	youtube-dl -q --write-description --skip-download --youtube-skip-dash-manifest -o "desc" $i 2>/dev/null
-        	mv desc.description description
-		cd $bypass
-		grep -v $i file.txt > tmpfile && mv tmpfile file.txt
-        	echo File Path : /srv/yt/downloads/"${titlename}"/"${titlename}"
-        	sudo echo "[`date "+%D %T"`] Video $i was downloaded. File Path : /srv/yt/downloads/"${titlename}"/"${titlename}"" >> /var/log/yt/download.log
+		if [[ $i =~ "https://www.youtube.com" ]]; then
+			sed -i '1d' file.txt
+        		titlename=`youtube-dl -e $i 2>&1`
+			if [[ $titlename =~ "ERROR" ]]; then
+				exit
+			else
+        			mkdir downloads/"${titlename}"
+        			cd "downloads/${titlename}" && youtube-dl -q -f mp4 -o "${titlename}.mp4" $i 2>/dev/null
+				echo "Video $i was downloaded."
+				youtube-dl -q --write-description --skip-download --youtube-skip-dash-manifest -o "desc" $i 2>/dev/null
+        			mv desc.description description
+				cd $bypass
+				echo File Path : /srv/yt/downloads/"${titlename}"/"${titlename}"
+				sudo echo "[`date "+%D %T"`] Video $i was downloaded. File Path : /srv/yt/downloads/"${titlename}"/"${titlename}"" >> /var/log/yt/download.log
+			fi
+		else
+			sed -i '1d' file.txt
+		fi
 	done
 else
 	if [[ -d /var/log/yt ]]; then
--- TP3/srv/yt/yt.sh
+++ TP3/srv/yt/yt.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 
 if [[ -d downloads && -d /var/log/yt ]]; then
-        titlename=`youtube-dl -e $1`
-        mkdir downloads/"${titlename}"
-        cd "downloads/${titlename}" && youtube-dl -q -f mp4 -o "${titlename}.mp4" $1 2>/dev/null
-	echo "Video $1 was downloaded."
-        youtube-dl -q --write-description --skip-download --youtube-skip-dash-manifest -o "desc" $1 2>/dev/null
-        mv desc.description description
-        echo File Path : /srv/yt/downloads/"${titlename}"/"${titlename}"
-        sudo echo "[`date "+%D %T"`] Video $1 was downloaded. File Path : /srv/yt/downloads/"${titlename}"/"${titlename}"" >> /var/log/yt/download.log
+	titlename=`youtube-dl -e $1 2>&1`
+        if [[ $titlename = "ERROR" ]]; then
+		echo "Video link is bad, retry.."
+                exit
+        else
+        	mkdir downloads/"${titlename}"
+        	cd "downloads/${titlename}" && youtube-dl -q -f mp4 -o "${titlename}.mp4" $1 2>/dev/null
+		echo "Video $1 was downloaded."
+        	youtube-dl -q --write-description --skip-download --youtube-skip-dash-manifest -o "desc" $1 2>/dev/null
+        	mv desc.description description
+        	echo File Path : /srv/yt/downloads/"${titlename}"/"${titlename}"
+        	sudo echo "[`date "+%D %T"`] Video $1 was downloaded. File Path : /srv/yt/downloads/"${titlename}"/"${titlename}"" >> /var/log/yt/download.log
+	fi
 else
 	if [[ -d /var/log/yt ]]; then
 		echo "Dossier downloads manquant, error.."                             
