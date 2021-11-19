17d38628235c81e1cd6ca5fb914ee726fb64e121
HyouKash <HyouKa14@protonmail.com>
2021-11-19 10:20:20 +0100

    TP3

--- TP3/srv/yt-v2/yt-v2.sh
+++ TP3/srv/yt-v2/yt-v2.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
+path="home/hyouka/Tp-Linux/TP3/"
 while :
 do
-	if [[ -d downloads && -d /var/log/yt ]]; then
+	if [[ -d ${path}srv/yt-v2/downloads && -d /var/log/yt ]]; then
 		bypass=`pwd`
-		for i in `cat file.txt | xargs`; do
+		for i in `cat ${path}srv/yt-v2/file.txt | xargs`; do
 			if [[ $i =~ "https://www.youtube.com" ]]; then
-				sed -i '1d' file.txt
+				sed -i '1d' ${path}srv/yt-v2/file.txt
 				titlename=`youtube-dl -e $i 2>&1`
 				if [[ $titlename =~ "ERROR" ]]; then
 					sudo echo "[`date "+%D %T"`] Video $i has an error : $titlename" >> /var/log/yt/download.log
-					exit
 				else
-					mkdir downloads/"${titlename}"
-					cd "downloads/${titlename}" && youtube-dl -q -f mp4 -o "${titlename}.mp4" $i 2>/dev/null
+					mkdir ${path}srv/yt-v2/downloads/"${titlename}"
+					cd "${path}srv/yt-v2/downloads/${titlename}" && youtube-dl -q -f mp4 -o "${titlename}.mp4" $i 2>/dev/null
 					echo "Video $i was downloaded."
 					youtube-dl -q --write-description --skip-download --youtube-skip-dash-manifest -o "desc" $i 2>/dev/null
 					mv desc.description description
@@ -21,14 +21,14 @@ do
 					sudo echo "[`date "+%D %T"`] Video $i was downloaded. File Path : /srv/yt/downloads/"${titlename}"/"${titlename}"" >> /var/log/yt/download.log
 				fi
 			else
-				sed -i '1d' file.txt
+				sed -i '1d' ${path}srv/yt-v2/file.txt
 			fi
 		done
 	else
 		if [[ -d /var/log/yt ]]; then
 			echo "Dossier downloads manquant, error.."                             
 			exit
-		elif [[ -d downloads ]]; then
+		elif [[ -d ${path}srv/yt-v2/downloads ]]; then
 			echo "Dossier /var/log/yt/downloads.log manquant, error.."
 			exit
 		else
